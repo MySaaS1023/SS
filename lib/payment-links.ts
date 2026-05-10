38c87c502286adf77d1394d7766713f91fbec678
MySaaS1023 <support@steadystartco.com>
2026-05-10 00:50:03 -0600

    stripe links update

--- lib/payment-links.ts
+++ lib/payment-links.ts
@@ -1,5 +1,5 @@
 export const PAYMENT_LINKS = {
-  starter: "https://buy.stripe.com/fZu00j5h8dNn38feEhd7q02",
-  business: "https://buy.stripe.com/14AaEX6lc10BbEL8fTd7q00",
-  premium: "https://buy.stripe.com/8x24gzfVM38J7ovao1d7q01",
+  starter: "https://buy.stripe.com/eVq6oHfVMbFfdMT53Hd7q03",
+  business: "https://buy.stripe.com/6oU8wPdNE8t37ov67Ld7q05",
+  premium: "https://buy.stripe.com/bJefZhcJA5gR9wDeEhd7q04",
 };
