21c11a7f9a1a6bc518eed20c51abd6b4650ef3bc
MySaaS1023 <support@steadystartco.com>
2026-06-17 14:57:45 -0600

    resize menu logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -12,11 +12,11 @@ export function Logo({ compact = false }: LogoProps) {
       <Image
         src="/logo.png"
         alt="Steady Start Logo"
-        width={72}
-        height={72}
+        width={92}
+        height={92}
         priority
         className={`h-auto object-contain ${
-          compact ? "w-[46px] sm:w-[52px] md:w-[56px]" : "w-[56px] sm:w-[62px] md:w-[72px]"
+          compact ? "w-[62px] sm:w-[68px] md:w-[74px]" : "w-[56px] sm:w-[62px] md:w-[72px]"
         }`}
       />
     </Link>
